1ebb278df7bfa6f4b6fb5bf7b3b7e3efb147260d
aiflow681 <chuwensong866@gmail.com>
2026-03-09 10:44:31 +0800

    Delete assets directory

--- assets/scripts/NinjaAssaultGame.ts
+++
@@ -1,7 +0,0 @@
-import { _decorator } from 'cc';
-import { NinjaRushPortGame } from './ninja_rush_port/NinjaRushPortGame';
-
-const { ccclass } = _decorator;
-
-@ccclass('NinjaAssaultGame')
-export class NinjaAssaultGame extends NinjaRushPortGame {}
